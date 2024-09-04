161ee67bffa21a3f7df80aced663a308d156a0f4
Karina <kbrilhante@gmail.com>
2024-09-03 22:26:48 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#define ex00
+
+/*** EXERCICIOS VARIAVEIS ***/
+
+#ifdef ex01
+/*
+	Crie um programa que recebe um número e retorna o último algarismo digitado
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0; 
+}
+#endif
+
+#ifdef ex02
+/*
+	Crie um programa que recebe 4 números e retorna a média desses números
+*/
+int main(int argc, char **argv)
+{
+	printf("");
+    getchar();
+	return 0; 
+}
+#endif
+
+#ifdef ex03
+/*
+	Troca Variáveis
+	Crie um programa que recebe duas variáveis e inverte os seus valores
+
+	por exemplo:
+	a: 13
+	b: 54
+
+	... depois de processar ...
+
+	a: 54
+	b: 13
+
+	IMPORTANTE: não queremos trocar a visualização e sim o valor das variáveis
+*/
+int main(int argc, char **argv)
+{
+	// TODO: declaração e inicialização das variáveis
+	int a, b;
+
+
+	// não apague nem modifique a linha abaixo
+	printf("A: %d | B: %d\n", a, b);
+
+	// TODO: troque os valores das variáveis
+
+	
+	// não apague nem modifique a linha abaixo
+	printf("A: %d | B: %d\n", a, b);
+    
+    getchar();
+	return 0; 
+}
+#endif
